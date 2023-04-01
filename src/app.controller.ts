30db91b7eb2d06c76c4827ca9081827331c14311
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2023-03-31 18:47:45 -0700

    Add endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,45 @@
-import { Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ExtractedOpportunity } from './schemas/extractedOpportunity.schema';
+import { SubmitURLsRequestDto } from './dtos/request/submitURLs.request.dto';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private eventEmitter: EventEmitter2) {}
 
-  @Get()
-  async findAllOpportunities(): Promise<ExtractedOpportunity[]> {
+  @Get('/opportunities')
+  async getOpportunities(): Promise<ExtractedOpportunity[]> {
     return await this.appService.getOpportunities();
   }
 
-  @Post('/submit-url')
-  async submitURL(@Query('url') url: string): Promise<any> {
+  @Post('/opportunities')
+  async submitURL(@Query('url') url: string): Promise<string> {
+    console.info('Submitting a new URL 🔗: ', url);
     this.appService
       .submitURL(url)
-      .then(result => console.log(result))
-      .catch(error => console.log(error));
-    return 'received url: ' + url;
+      .then()
+      .catch(error => {
+        this.eventEmitter.emit('opportunity.extraction.pool.release');
+        console.log(error);
+      });
+
+    return url;
+  }
+
+  @Post('/opportunities/multiple')
+  async submitURLs(@Body() urlsRequestDto: SubmitURLsRequestDto): Promise<string[]> {
+    console.info('Submitting a list of URLs 🔗📃: ', urlsRequestDto);
+    urlsRequestDto.urls.forEach(url => {
+      this.appService
+        .submitURL(url)
+        .then()
+        .catch(error => {
+          this.eventEmitter.emit('opportunity.extraction.pool.release');
+          console.log(error);
+        });
+    });
+
+    return urlsRequestDto.urls;
   }
 }
